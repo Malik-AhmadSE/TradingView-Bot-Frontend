c32b782764e94c5d1e26de544c30e302b41e1416
Malik-AhmadSE <ahmadali009ali008@gmail.com>
2025-01-19 15:45:51 +0500

    project setup and component developing

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+     <h2 className="text-center text-base/7 font-semibold text-indigo-600">TradingView Bot</h2>
+        <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+          PrimeX -- Algo
+        </p>
+    </>
+  )
+}
+
+export default Header
