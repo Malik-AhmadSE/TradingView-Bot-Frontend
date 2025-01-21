7a0156144b77cdb268550c9d469db071c983693d
zohaibislam03 <zohaibislam03@gmail.com>
2025-01-21 19:08:38 +0500

    Made the home layout clean and responsive

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { SideBar, Header } from "../components";
+import { SideBar, Navbar } from "../components";
+
 function HomeLayout() {
-    return (
-        <>
-            <div className="bg-gray-50 w-full h-full py-10 flex flex-col justify-center items-center">
-                <div className="mx-auto w-full max-w-7xl  px-6 lg:max-w-7xl lg:px-8">
-                    <Header />
-                    <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-5 lg:grid-rows-2 w-full">
-                        <div className="relative lg:row-span-full col-span-1 ">
-                            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
-                            <div className="relative flex h-full flex-col overflow-hidden shadow-lg shadow-gray-300 rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                                <SideBar />
-                            </div>
-                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
-                        </div>
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div className="min-h-screen flex flex-col sm:flex-row">
+      <div className="hidden sm:block sm:w-64">
+        <SideBar />
+      </div>
+
+      <div className="flex-1 flex flex-col">
+        <div className="sm:hidden">
+          <Navbar />
+        </div>
+
+        <div className="p-4 sm:p-2">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeLayout;
