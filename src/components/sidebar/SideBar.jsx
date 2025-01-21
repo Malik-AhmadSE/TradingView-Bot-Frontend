20c85ba8be3a1c9018be50431e5727385eaa7eeb
zohaibislam03 <zohaibislam03@gmail.com>
2025-01-21 19:07:55 +0500

    Updated the side bar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
-import { FaHome} from 'react-icons/fa';
-import { FaCogs } from "react-icons/fa";
-function SideBar() {
-  return (
-    <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-      <p className="mt-2 text-lg font-bold tracking-tight text-gray-950 text-center max-lg:text-center">
-        Menu
-      </p>
+import React from "react";
+import { FaHome, FaCogs } from "react-icons/fa";
+import { List, ListItem, ListItemPrefix, Card } from "@material-tailwind/react";
+import { useNavigate } from "react-router";
 
-      {/* Menu Links with Icons */}
-      <div className="mt-6 space-y-4">
-        {/* Home Link */}
-        <a
-          href="/home"
-          className="flex items-center space-x-3 text-gray-700 hover:text-gray-900"
-        >
-          <FaHome className="text-xl" />
-          <span className="text-lg font-medium">Home</span>
-        </a>
-        
-        <a
-          href="/settings"
-          className="flex items-center space-x-2 text-gray-700 hover:text-gray-900"
-        >
-          <FaCogs className=" text-xl" />
-          <span className="text-lg font-medium">Setting</span>
-        </a>
+function SideBar() {
+  const navigate = useNavigate();
+  return (
+    <Card className="hidden sm:block w-64 h-screen bg-gradient-to-b from-[#00344C] via-[black] to-[#00405B] text-white shadow-lg rounded-r-2xl rounded-l-none">
+      <div className="px-3 pb-3 pt-8">
+        <p className="mt-2 text-lg font-bold tracking-tight text-center text-white">
+          Dashboard
+        </p>
+
+        <List className="mt-6">
+          <ListItem
+            className="hover:bg-gray-800 rounded-lg"
+            onClick={() => navigate("/home")}
+          >
+            <ListItemPrefix>
+              <FaHome className="text-white text-xl" />
+            </ListItemPrefix>
+            <a className="text-white hover:text-gray-400">Home</a>
+          </ListItem>
+
+          <ListItem
+            className="hover:bg-gray-800 rounded-lg"
+            onClick={() => navigate("/settings")}
+          >
+            <ListItemPrefix>
+              <FaCogs className="text-white text-xl" />
+            </ListItemPrefix>
+            <a className="text-white hover:text-gray-400">Settings</a>
+          </ListItem>
+        </List>
       </div>
-    </div>
+    </Card>
   );
 }
 
